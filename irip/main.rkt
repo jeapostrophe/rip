7fa609c06e3ed0a00efb0b15f8e2de859a503046
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-11 19:04:26 -0700

    almost

--- irip/main.rkt
+++ irip/main.rkt
@@ -9,8 +9,9 @@
 (struct v:bool val (b) #:transparent)
 (struct v:null val () #:transparent)
 (struct v:cons val (car cdr) #:transparent)
-(struct v:prim val (prim) #:transparent)
+(struct v:prim val (id prim) #:transparent)
 (struct v:clo val (args body env) #:transparent)
+(struct v:abstract val (env expr) #:transparent)
 
 (define parse-val
   (match-lambda
@@ -30,6 +31,7 @@
 (struct e:if expr (c t f) #:transparent)
 (struct e:app expr (fun args) #:transparent)
 (struct e:id expr (id) #:transparent)
+(struct e:abstract expr (label) #:transparent)
 
 (define ->racket
   (match-lambda
@@ -45,23 +47,29 @@
   (λ (x) (racket-> (f (->racket x)))))
 
 (define PRIMITIVES
-  (hasheq '+ (prim-bin +)
-          '- (prim-bin -)
-          '* (prim-bin *)
-          '/ (prim-bin /)
-          'zero? (prim-una zero?)
-          '= (prim-bin =)
-          'not (prim-una not)
-          'or (prim-bin (λ (x y) (or x y)))
-          'and (prim-bin (λ (x y) (and x y)))
-          'number? (compose racket-> v:num?)
-          'boolean? (compose racket-> v:bool?)
-          'null? (compose racket-> v:null?)
-          'cons v:cons
-          'cons? (compose racket-> v:cons?)
-          'car v:cons-car
-          'cdr v:cons-cdr
-          'procedure? (λ (f) (racket-> (or (v:clo? f) (v:prim? f))))))
+  (hasheq
+   '+ (prim-bin +)
+   '- (prim-bin -)
+   '* (prim-bin *)
+   '/ (prim-bin /)
+   'zero? (prim-una zero?)
+   '= (prim-bin =)
+   'not (prim-una not)
+   'or (prim-bin (λ (x y) (or x y)))
+   'and (prim-bin (λ (x y) (and x y)))
+   'number? (compose racket-> v:num?)
+   'boolean? (compose racket-> v:bool?)
+   'null? (compose racket-> v:null?)
+   'cons v:cons
+   'cons? (compose racket-> v:cons?)
+   'car v:cons-car
+   'cdr v:cons-cdr
+   'procedure? (λ (f) (racket-> (or (v:clo? f) (v:prim? f))))
+   'check
+   (λ (x y)
+     (if (equal? x y)
+       (v:bool #t)
+       (error 'check "Failed: ~e vs ~e" x y)))))
 
 (define MACROS
   (make-hasheq))
@@ -74,50 +82,100 @@
     (e:lam args (parse-expr body))]
    [(list 'if c t f)
     (e:if (parse-expr c) (parse-expr t) (parse-expr f))]
+   [(list '_ any)
+    (e:abstract any)]
    [(and macro-invoke (list-rest (? (λ (id) (hash-has-key? MACROS id)) macro) _))
     (parse-expr ((hash-ref MACROS macro) macro-invoke))]
    [(list fun args ...)
     (e:app (parse-expr fun) (map parse-expr args))]
    [(? symbol? id)
     (if (hash-has-key? PRIMITIVES id)
-      (e:val (v:prim (hash-ref PRIMITIVES id)))
+      (e:val (v:prim id (hash-ref PRIMITIVES id)))
       (e:id id))]))
 
 (define (v:false? v)
   (and (v:bool? v) (not (v:bool-b v))))
 
-(define (v:apply prim-or-clo arg-vs)
+(define (v:apply prim-or-clo arg-vs ok fail)
+  (define fe
+    (e:app (e:val prim-or-clo)
+           (map e:val arg-vs)))
   (match prim-or-clo
     [(v:clo arg-ids body env)
-     (unless (= (length arg-vs) (length arg-ids))
-       (error 'v:apply "Wrong number of arguments"))
-     (interp body
-             (for/fold ([env env])
-                 ([ai arg-ids]
-                  [av arg-vs])
-               (hash-set env ai av)))]
-    [(v:prim prim)
-     (apply prim arg-vs)]))
+     (if (= (length arg-vs) (length arg-ids))
+       (interp body
+               (for/fold ([env env])
+                   ([ai arg-ids]
+                    [av arg-vs])
+                 (hash-set env ai av))
+               ok
+               fail)
+       (fail (format "Wrong number of arguments: ~e vs ~e"
+                     (length arg-vs) (length arg-ids))
+             fe))]
+    [(v:prim _ prim)
+     (let/ec outside
+       (ok
+        (with-handlers ([exn:fail?
+                         (λ (x)
+                           (outside (fail (exn-message x) fe)))])
+          (apply prim arg-vs))))]))
 
-(define (interp e env)
+(define (snoc l x)
+  (append l (list x)))
+
+;;   ok : val -> ans
+;; fail : msg expr -> ans
+(define (interp e env ok fail)
   (match e
+    [(e:abstract label)
+     (ok (v:abstract env (e:abstract label)))]
     [(e:id i)
-     (hash-ref env i)]
+     (if (hash-has-key? env i)
+       (ok (hash-ref env i))
+       (fail (format "Unknown identifier: ~e" i) (e:id i)))]
     [(e:val v)
-     v]
+     (ok v)]
     [(e:if c t f)
-     (if (v:false? (interp c env))
-       (interp f env)
-       (interp t env))]
+     (interp
+      c env
+      (λ (cv)
+        (if (v:false? cv)
+          (interp f env ok fail)
+          (interp t env ok fail)))
+      (λ (r ce)
+        (fail r (e:if ce t f))))]
     [(e:lam args body)
-     (v:clo args body env)]
+     (ok (v:clo args body env))]
     [(e:app fun args)
-     (v:apply (interp fun env)
-              (for/list ([a args])
-                (interp a env)))]))
+     (interp
+      fun env
+      (λ (fv)
+        (define (interp-args args-v args-e)
+          (if (empty? args-e)
+            (v:apply fv args-v ok fail)
+            (interp (first args-e)
+                    env
+                    (λ (arg-v)
+                      (interp-args (snoc args-v arg-v)
+                                   (rest args-e)))
+                    (λ (r arg-e)
+                      (fail
+                       r
+                       (e:app (e:val fv)
+                              (append
+                               (map e:val args-v)
+                               (cons arg-e (rest args-e)))))))))
+        (interp-args empty args))
+      (λ (r fe)
+        (fail r (e:app fe args))))]))
 
 (define (interp* se)
-  (interp (parse-expr se) (hasheq)))
+  (interp
+   (parse-expr se)
+   (hasheq)
+   (λ (sev) sev)
+   (λ (r see) (error 'fail "~a: ~e" r see))))
 
 (module+ test
   (chk
@@ -190,3 +248,79 @@
 
   (chk
    (interp* `(,:sum '(1 2 3 4))) (parse-val '10)))
+
+(define-macro! letrec
+  (match-lambda
+   [(list 'letrec (list (list fun-id fun-expr) ...) body)
+    (list 'let (for/list ([fun-id fun-id] [fun-expr fun-expr])
+                 (list fun-id
+                       (list :Y
+                             (list 'λ (list fun-id) fun-expr))))
+          body)]))
+
+(define-macro! begin
+  (match-lambda
+   [(list 'begin before ... last)
+    (list 'let
+          (for/list ([before before])
+            (list (gensym 'begin) before))
+          last)]))
+
+(define (interactive se)
+  (interp
+   (parse-expr se)
+   (hasheq)
+   (λ (sev) sev)
+   (λ (r see)
+     (print-context see))))
+
+(require racket/pretty)
+
+(define unparse
+  (match-lambda
+   [(e:app fun args)
+    (list* (unparse fun) (map unparse args))]
+   [(e:lam args body)
+    (list 'λ args (unparse body))]
+   [(v:clo args body env)
+    (list '<closure> (unparse env) args (unparse body))]
+   [(v:prim id _)
+    id]
+   [(e:val (and (or (? v:clo?) (? v:prim?)) v))
+    (unparse v)]
+   [(e:val v)
+    (list 'quote (unparse v))]
+   [(v:cons a d)
+    (cons (unparse a) (unparse d))]
+   [(v:num n)
+    n]
+   [(v:null)
+    null]
+   [(v:bool b)
+    b]
+   [(v:abstract env e)
+    (list '<abstract>
+          (unparse env)
+          (unparse e))]
+   [(e:abstract id)
+    (list '_ id)]
+   [(e:id id)
+    id]
+   [(? hash? env)
+    (for/list ([(k v) env])
+      (list k '-> (unparse v)))]))
+
+(define (print-context e)
+  (pretty-display (unparse e)))
+
+(module+ test
+  (interactive
+   `(letrec ([sum (λ (l) (_ sum-definition))])
+      (begin (check (sum '(1 2 3 4)) '10)
+             (check (sum '(1 1 1 1)) '4)
+             (check (sum '(1 1 1 1 5 6)) '15)
+             (check (sum '()) '0)
+             (check (sum '(1)) '1)
+             (check (sum '(2)) '2)
+             (check (sum '(1 2)) '3)
+             '#t))))
