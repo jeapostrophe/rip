49a5f60f629020a3ecf9c305ebdf7eea6efd1547
cmk49 <christinemk03@gmail.com>
2014-04-04 12:28:01 -0600

    fixing looping issue

--- grip/interact.rkt
+++ grip/interact.rkt
@@ -10,10 +10,13 @@
         [o (in-list options)])
     (printf " ~a. ~a\n" i (car o)))
   (define in (read))
-  (if (and (number? in)
-           (< 0 in (+ 1 (length options))))
-      ((cdr (list-ref options (- in 1))))
-      (printf "Invalid user input")))
+  (cond 
+    [(and (number? in)
+          (< 0 in (+ 1 (length options))))
+     ((cdr (list-ref options (- in 1))))]
+    [else
+     (printf "Invalid user input")
+     (interact* options)]))
 
 (provide 
  interact)
